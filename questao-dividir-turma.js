e20bcd093ca3dc0b3a555a97925b619809c98c76
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-08-29 19:29:14 -0300

    atividade realizada com sucesso

---
+++ questao-dividir-turma.js
@@ -0,0 +1,16 @@
+const alunos = [
+    "Carolina",
+    "Mateus",
+    "Isabela",
+    "Rafael",
+    "Bianca",
+    "Henrique",
+    "Luana",
+    "Fernando",
+    "Aline"
+];
+
+const lista1 = alunos.slice(0, alunos.length / 2);
+const lista2 = alunos.slice(alunos.length / 2);
+
+console.log(lista1, lista2);
