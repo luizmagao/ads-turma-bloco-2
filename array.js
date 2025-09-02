32f17f04b5adaf38c3c0e7d24e5bc28677d33caf
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-09-02 18:45:52 -0300

    WP: usando indexof e includes

--- array.js
+++ array.js
@@ -1,6 +1,23 @@
+const estudantes = [
+    'luiz',
+    'lins',
+    'monteiro',
+    'junior'
+];
 
-const esparso = [10, 6.5, 'oi', [7], {a:'b'}];
-console.log(esparso);
-console.log(esparso.length);
+const medias = [
+    10,
+    9.5,
+    8.3,
+    7
+];
 
+const funcao = function(aluno) {
+    if( estudantes.includes(aluno) ) {
+        let index = estudantes.indexOf(aluno); 
+        return `A média de ${aluno}: ${medias[index]}`;
+    }
+    return 'O aluno não foi encontrado';
+} 
 
+console.log(funcao('junior999'));
