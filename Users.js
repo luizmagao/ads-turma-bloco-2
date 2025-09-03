25a02a4f7d0a8e39c991c3f94dafdebac081801b
luizlins <luizlins@luizlins.local>
2025-09-03 09:35:54 -0300

    WP: Class - parte 01

---
+++ Users.js
@@ -0,0 +1,19 @@
+class User {
+
+    constructor(nome, email, nascimento, role, ativo =  true) {
+        this.nome = nome
+        this.email = email
+        this.nascimento = nascimento
+        this.role = role || "estudante"
+        this.ativo = ativo
+    }
+
+    exibirInfos() {
+        return `${this.nome}, ${this.email}`
+    }
+
+}
+
+const novoUser = new User('Juliana', 'j@j.com', '1985-10-27')
+console.log(novoUser)
+console.log(novoUser.exibirInfos())
