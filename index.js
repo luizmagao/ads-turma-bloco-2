44463dcfbcf91e8db226d95f4cc125d34aed71c9
luizlins <luizlins@luizlins.local>
2025-09-03 14:27:40 -0300

    WP: inicio

--- index.js
+++
@@ -1,6 +0,0 @@
-import User from "./Users.js"
-// import Admin from "./Admin.js"
-// import Docente from "./Docente.js"
-
-const novoUser = new User("juliana", "j@j.com")
-console.log(novoUser.exibirInfos())
