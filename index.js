f2ef4abee4c2bac155da844899a386d2b230433e
luizlins <luizlins@luizlins.local>
2025-09-03 14:44:20 -0300

    WP: addEventListener - parte 02

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const inputItem = document.getElementById('input-item')
+
+const botaoAdicionar = document.getElementById('adicionar-item')
+
+botaoAdicionar.addEventListener('click', function (evento) {
+  return console.log(evento)
+})
